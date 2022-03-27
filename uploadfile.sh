2feb9efa7e540b32b3ddde88588a09fd8e023314
Manojsaisubbarao-Devops <102476916+Manojsaisubbarao-Devops@users.noreply.github.com>
2022-03-27 21:12:07 +0530

    Update uploadfile.sh

--- uploadfile.sh
+++ uploadfile.sh
@@ -5,7 +5,7 @@ if [ ! -z $first ]; then
 export repoPath="$second"
 export filepath=$(find . -name $first)
 export FILE_NAME=$(echo $filepath | tr -s '/' '\n' | tail -1)
-curl --insecure -H "X-JFrog-Art-Api:apikey" -T $filepath "https://artifacthub-phx.oci.oraclecorp.com/artifactory/cegbu-bfs-generic/$repoPath/$FILE_NAME" --noproxy artifacthub-phx.oci.oraclecorp.com
+curl --insecure -H "X-JFrog-Art-Api:apikey" -T $filepath "https://jfrogurl.com/artifactory/team-generic-artifactory/$repoPath/$FILE_NAME"
 
 else
 
